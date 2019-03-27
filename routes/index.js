f4ccbaeccaa661d42e525f8d783db6fd5df6c0b8
Nathan C <32592701+awakedev@users.noreply.github.com>
2019-03-27 12:51:48 +0000

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+var multer  = require('multer');
+var upload = multer({storage: storage});
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './public/images');
+    },
+    filename: (req, file, cb) => {
+      console.log(file);
+      var filetype = '';
+      if(file.mimetype === 'image/gif') {
+        filetype = 'gif';
+      }
+      if(file.mimetype === 'image/png') {
+        filetype = 'png';
+      }
+      if(file.mimetype === 'image/jpeg') {
+        filetype = 'jpg';
+      }
+      cb(null, 'image-' + Date.now() + '.' + filetype);
+    }
+});
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/upload',upload.single('file'),function(req, res, next) {
+  console.log(req.file);
+  if(!req.file) {
+    res.status(500);
+    return next(err);
+  }
+  res.json({ fileUrl: 'http://192.168.0.7:3000/images/' + req.file.filename });
+})
+
+module.exports = router;
